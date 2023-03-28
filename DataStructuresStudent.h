e11c998d6f31f319268d34a5e62fd4a346a42ca2
Zia Uddin <77499979+ziaKhan1995@users.noreply.github.com>
2023-03-28 15:16:35 +0500

    Update DataStructuresStudent.h

--- DataStructuresStudent.h
+++ DataStructuresStudent.h
@@ -1,38 +1,41 @@
-#pragma once
-#include "Time.h"
+#pragma once#include "Time.h"
 
 #include <iostream>
+
 #include <string>
+
 #include <fstream>
+
 using namespace std;
 
-class DataStructuresStudent
-{
-    string firstName;
-    string lastName;
-    Time enterTime;
-    int numQuestions;
-    int numAwnsered;
-    int laptopSerialNum;
+class DataStructuresStudent {
+  string firstName;
+  string lastName;
+  Time enterTime;
+  int numQuestions;
+  int numAwnsered;
+  int laptopSerialNum;
   public:
     DataStructuresStudent();
-    void setTime(int minute);
-    void setQuestions(int n);
-    void setAwnsered(int n);
-    void setSerialNum(int serial);
-    void setFirst(string first);
-    void setLast(string last);
-    Time getTime();
-    int getQuestions();
-    int getAwnsered();
-    int getSerialNum();
-    string getFirst();
-    string getLast();
-    string getName();
-    float perc(); //gives percentage of awnsers
-    bool isSatisfy();
-    bool operator==(DataStructuresStudent&);
-    int inc(){return ++numAwnsered;}
-    friend ostream& operator<<(ostream& a, DataStructuresStudent s);
-    friend ifstream& operator>>(ifstream&, DataStructuresStudent&);
+  void setTime(int minute);
+  void setQuestions(int n);
+  void setAwnsered(int n);
+  void setSerialNum(int serial);
+  void setFirst(string first);
+  void setLast(string last);
+  Time getTime();
+  int getQuestions();
+  int getAwnsered();
+  int getSerialNum();
+  string getFirst();
+  string getLast();
+  string getName();
+  float perc(); //gives percentage of awnsers
+  bool isSatisfy();
+  bool operator == (DataStructuresStudent & );
+  int inc() {
+    return ++numAwnsered;
+  }
+  friend ostream & operator << (ostream & a, DataStructuresStudent s);
+  friend ifstream & operator >> (ifstream & , DataStructuresStudent & );
 };
